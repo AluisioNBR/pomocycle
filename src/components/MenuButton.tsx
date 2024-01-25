65d2201598db7ec045a228bc587778761eddfa08
AluisioNBR <lulu70638@gmail.com>
2024-01-25 19:13:02 -0300

    style: menu button

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -2,8 +2,16 @@ import { Button, Image } from '@chakra-ui/react'
 
 export function MenuButton() {
 	return (
-		<Button title="Menu">
-			<Image src="menuIcon.png" alt="Menu" />
+		<Button
+			title="Menu"
+			bg="#fff"
+			w="3.5rem"
+			h="3.5rem"
+			pos="absolute"
+			top=".5rem"
+			left=".5rem"
+		>
+			<Image src="menuIcon.png" alt="Menu" w="4rem" h="1.25rem" />
 		</Button>
 	)
 }
