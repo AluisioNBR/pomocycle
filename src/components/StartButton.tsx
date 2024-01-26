f11c8966a0e32da2cc034af3407cd14d89ccc52b
AluisioNBR <lulu70638@gmail.com>
2024-01-26 19:44:44 -0300

    style: start button

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -2,7 +2,17 @@ import { Button, Image } from '@chakra-ui/react'
 
 export function StartButton() {
 	return (
-		<Button>
+		<Button
+			display="flex"
+			alignItems="center"
+			justifyContent="center"
+			w="5rem"
+			h="5rem"
+			paddingLeft="1.5rem"
+			bgGradient="linear(to-b, #9B33CC, #7933CC)"
+			_hover={{ backgroundColor: '#7933CC' }}
+			rounded={999}
+		>
 			<Image src="playIcon.png" alt="Botão de Início" title="Botão de Início" />
 		</Button>
 	)
